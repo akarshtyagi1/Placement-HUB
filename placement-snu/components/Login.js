44825a121024734c77c96159e5f3e97ca9808c78
akarshtyagi1 <akarshtyagi1@gmail.com>
2022-04-22 20:49:51 +0530

    login page ui done

--- placement-snu/components/Login.js
+++ placement-snu/components/Login.js
@@ -1,14 +1,30 @@
 import styles from "../styles/login.module.scss";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
+import Logo from "../public/app-logo.png";
 
 export default function Login({ providerId }) {
   return (
-    <div className={styles.login_wrapper}>
-      <div className={styles.login}>
-        <div className={styles.logo_wrapper}>{/* <Image /> */}</div>
-        <p>Login with your SNU Email only.</p>
-        <button onClick={() => signIn(providerId)}>Login</button>
+    <>
+      <div className={styles.LoginWrapper}>
+        <div className={styles.Loginform}>
+          <div className={styles.LogoWrapper}>
+            <div className={styles.Logo}>
+              <Image src={Logo} />
+            </div>
+          </div>
+          <div className={styles.buttonSection}>
+            <p>Enter using your SNU ID</p>
+            <button
+              onClick={() => {
+                signIn(providerId);
+              }}
+            >
+              Login
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
-}clear
+}
