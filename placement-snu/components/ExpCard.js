45fcfac83a9abcf53b9f0e0bc594596285278316
akarshtyagi1 <akarshtyagi1@gmail.com>
2022-04-24 17:09:46 +0530

    experience panel done

--- placement-snu/components/ExpCard.js
+++ placement-snu/components/ExpCard.js
@@ -1,21 +1,13 @@
 import styles from "../styles/expCard.module.scss";
 
-export default function ExpCard() {
+export default function ExpCard({ user, title, content, year }) {
   return (
     <div className={styles.experience}>
-      <h2>GoldMan Sachs Interview Experience </h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <h2>{title}</h2>
+      <p>{content}</p>
       <div>
-        <p>by Akarsh Tyagi</p>
-        <p>2022</p>
+        <p>by {user}</p>
+        <p>{year}</p>
       </div>
     </div>
   );
--- placement-snu/pages/api/user.js
+++ placement-snu/pages/api/user.js
@@ -1,14 +1,17 @@
-import {db} from "../../utils/firebase"
-import { collection, getDoc } from "firebase/firestore";
+import { getSession } from "next-auth/react";
 
 export default async function handler(req, res){
     if(req.method === 'POST'){
-        const userColRef = collection(db,'users');
-        const data = await getDoc(userColRef)
-
-        const userData = data.map
-
-    }else{
-        res.status(405).send('This HTTP method is not allowed');
+        const session = await getSession({req})
+        if(session){
+            const user = JSON.stringify(session.user);
+            res.status(200).json(session.user);
+            return;
+        }
+        res.status(201).send(null);
+        return;
+    }
+    else{
+        res.status(405).send('<h1>This HTTP method is not allowed</h1>');
     }
 }    
